675140cf2a2be37b8817d9bbdf3175f08d5da2a9
Ahmedgafsi <ahmed.gafsi.2001@gmail.com>
2024-05-25 21:48:54 +0100

    finall

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+//root 
 //DO NOT TOUCH THIS FILE PLEASE
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
 
